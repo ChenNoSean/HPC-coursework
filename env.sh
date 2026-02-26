65eb182e8f8efd2852b179bb6872527ddb0a8a94
midir9t b35cg <midir9t.b35cg@login01.head.cm.i3.isambard.ac.uk>
2026-02-26 12:53:16 +0000

    Final submission version

--- env.sh
+++ env.sh
@@ -1,2 +1,12 @@
 # Add any `module load` or `export` commands that your code needs to
 # compile and run to this file.
+#!/bin/bash
+module purge
+module load PrgEnv-gnu
+
+
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-144}
+export OMP_PLACES=cores
+export OMP_PROC_BIND=spread
+export OMP_SCHEDULE=static
+export OMP_DYNAMIC=false
